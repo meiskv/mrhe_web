0f914b14a48713f38256a5863ccc247619a01899
Mikhail Villamor <miks.villamor@gmail.com>
2016-07-28 15:18:29 +0400

    no message

--- database/migrations/2016_07_24_095923_create_table_landsearch.php
+++ database/migrations/2016_07_24_095923_create_table_landsearch.php
@@ -6,26 +6,13 @@ use Illuminate\Database\Migrations\Migration;
 class CreateTableLandsearch extends Migration
 {
     /**
-     * Run the migrations
+     * 
      *
      * @return void
      */
     public function up()
     {
-        Schema::create('landsearch', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('land_number_id')->unique();
-            $table->string('land_use');
-            $table->string('plan');
-            $table->string('beneficiary');
-            $table->string('owners_name');
-            $table->string('longitude');
-            $table->string('latitude');
-            $table->string('areasqft');
-            $table->string('areasqm');
-            $table->string('areaname');
-            $table->timestamps();
-        });
+        
     }
 
     /**
@@ -35,6 +22,6 @@ class CreateTableLandsearch extends Migration
      */
     public function down()
     {
-        Schema::drop('landsearch');
+       // Schema::drop('landsearch');
     }
 }
